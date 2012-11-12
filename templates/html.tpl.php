216bdfcc5aa60abc0696902b6d239182e0c700e8
Anna Gerber <anna.m.gerber@gmail.com>
2012-11-12 14:38:32 +1000

    Initial checkin of AustESE theme Based on Twitter bootstrap theme

---
+++ templates/html.tpl.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>
+  <?php print $styles; ?>
+  <!-- HTML5 element support for IE6-8 -->
+  <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+<body class="<?php print $classes; ?>" <?php print $attributes;?>>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+  <?php print $scripts; ?>
+  <script type="text/javascript">
+    jQuery(document).ready(function(){
+       jQuery('.dropdown-toggle').dropdown()
+    });
+  </script>
+</body>
+</html>
